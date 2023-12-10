ed8a1c08c6675eb70530b41826f277645bc1fc14
Taimur Naziri <taimurknaziri@gmail.com>
2023-12-10 10:10:55 -0500

    Add Make DOM script

--- mkdm.sh
+++ mkdm.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 # Use this script to make an HTML document with all the necessary elements
-touch ~/Desktop/index.html
-
 echo "<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale='1.0'">
     <title>Document</title>
 </head>
 <body>
     
 </body>
-</html>" >> ~/Desktop/index.html
+</html>" > "index.html"
 
 
