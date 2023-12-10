b811f1a26354321e3e0ea063d7865e8e9223df94
Taimur Naziri <taimurknaziri@gmail.com>
2023-12-10 09:08:12 -0500

    Sun Dec 10 09:08:12 EST 2023

---
+++ mkdm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Use this script to make an HTML document with all the necessary elements
+touch ~/Desktop/index.html
+
+echo "<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>" >> ~/Desktop/index.html
+
+
